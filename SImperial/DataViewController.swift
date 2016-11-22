4fdc3771dcd6fbbb4e8cde204b1dda3124585b15
Petteri Parkkila <petteri.parkkila@reaktor.fi>
2016-11-22 16:03:25 -0500

    Add initial model architechture and conversions;
    * Unit changing not implemented yet

--- SImperial/DataViewController.swift
+++ SImperial/DataViewController.swift
@@ -10,25 +10,71 @@ import UIKit
 
 class DataViewController: UIViewController {
 
+    @IBOutlet weak var siLabel: UILabel!
+    @IBOutlet weak var imperialLabel: UILabel!
     @IBOutlet weak var dataLabel: UILabel!
-    var dataObject: String = ""
-
+    @IBOutlet weak var siTextField: UITextField!
+    @IBOutlet weak var imperialTextField: UITextField!
+    var dataObject: Measurement? = nil
+    var selectedSiUnit: String? = nil
+    var selectedImperialUnit: String? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
+        if let measurement = dataObject {
+            self.dataLabel!.text = measurement.header
+            let siUnit = measurement.SIValues[0]
+            let imperialUnit = measurement.imperialValues[0]
+            self.siLabel!.text = siUnit.object(forKey: "name") as! String?
+            self.imperialLabel!.text = imperialUnit.object(forKey: "name") as! String?
+            selectedSiUnit = siUnit.object(forKey: "abbreviation") as! String?
+            selectedImperialUnit = imperialUnit.object(forKey: "abbreviation") as! String?
+        } else {
+            self.dataLabel!.text = ""
+            self.siLabel!.text = ""
+            self.imperialLabel!.text = ""
+        }
     }
-
-
+    
+    
+    @IBAction func onSiChanged(_ sender: Any) {
+        if let measurement = dataObject {
+            if let siText = siTextField.text {
+                if let siValue = Double(siText) {
+                    let imperialValue = measurement.convert(fromUnit: self.selectedSiUnit!, toUnit: self.selectedImperialUnit!, fromValue: siValue)
+                    imperialTextField.text = String(round(10 * imperialValue) / 10)
+                    
+                } else {
+                    imperialTextField.text = ""
+                }
+            } else {
+                imperialTextField.text = ""
+            }
+        }
+    }
+    
+    @IBAction func onImperialChanged(_ sender: Any) {
+        if let measurement = dataObject {
+            if let imperialText = imperialTextField.text {
+                if let imperialValue = Double(imperialText) {
+                    let siValue = measurement.convert(fromUnit: self.selectedImperialUnit!, toUnit: self.selectedImperialUnit!, fromValue: imperialValue)
+                    siTextField.text = String(round(10 * siValue) / 10)
+                    
+                } else {
+                    siTextField.text = ""
+                }
+            } else {
+                siTextField.text = ""
+            }
+        }
+    }
 }
 
--- SImperial/ModelController.swift
+++ SImperial/ModelController.swift
@@ -8,44 +8,400 @@
 
 import UIKit
 
-/*
- A controller object that manages a simple model -- a collection of month names.
- 
- The controller serves as the data source for the page view controller; it therefore implements pageViewController:viewControllerBeforeViewController: and pageViewController:viewControllerAfterViewController:.
- It also implements a custom method, viewControllerAtIndex: which is useful in the implementation of the data source methods, and in the initial configuration of the application.
- 
- There is no need to actually create view controllers for each page in advance -- indeed doing so incurs unnecessary overhead. Given the data model, these methods create, configure, and return a new view controller on demand.
- */
+protocol Measurement {
+    var header: String { get }
+    var SIValues: [NSDictionary] { get }
+    var imperialValues: [NSDictionary] { get }
+    func convert(fromUnit: String, toUnit: String, fromValue: Double) -> Double
+}
 
+struct Temperature: Measurement {
+    var header: String {
+        return "Temperature"
+    }
+    
+    var SIValues: [NSDictionary] {
+        return [["name": "Celcius", "abbreviation": "ºC"]]
+    }
+    
+    var imperialValues: [NSDictionary] {
+        return [["name": "Fahrenheit", "abbreviation": "ºF"]]
+    }
+
+    internal func convert(fromUnit: String, toUnit: String, fromValue: Double) -> Double {
+        switch fromUnit {
+        case "ºC":
+            return fromValue * 9 / 5 + 32
+        case "ºF":
+            return (fromValue - 32) * 5 / 9
+        default:
+            return fromValue
+        }
+    }
+}
+
+struct Weight: Measurement {
+    var header: String {
+        return "Weight"
+    }
+    
+    var SIValues: [NSDictionary] {
+        return [["name": "Gram", "abbreviation": "g"],
+                ["name": "Kilogram", "abbreviation": "kg"]]
+    }
+    
+    var imperialValues: [NSDictionary] {
+        return [["name": "Stone", "abbreviation": "st"],
+                ["name": "Pound", "abbreviation": "lb"],
+                ["name": "Ounce", "abbreviation": "oz"]]
+    }
+
+    internal func convert(fromUnit: String, toUnit: String, fromValue: Double) -> Double {
+        switch fromUnit {
+        case "g":
+            return convertGram(toUnit: toUnit, fromValue: fromValue)
+        case "kg":
+            return convertKilogram(toUnit: toUnit, fromValue: fromValue)
+        case "st":
+            return convertStone(toUnit: toUnit, fromValue: fromValue)
+        case "lb":
+            return convertPound(toUnit: toUnit, fromValue: fromValue)
+        case "oz":
+            return convertOunce(toUnit: toUnit, fromValue: fromValue)
+        default:
+            print("NO CONVERSION FOR \(fromUnit). Plz implement!")
+            return fromValue
+        }
+    }
+
+    func convertGram(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "st":
+            return fromValue * 0.000157473
+        case "lb":
+            return fromValue * 0.00220462
+        case "oz":
+            return fromValue * 0.035274
+        default:
+            return fromValue
+        }
+    }
+
+    func convertKilogram(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "st":
+            return fromValue * 0.157473
+        case "lb":
+            return fromValue * 2.20462
+        case "oz":
+            return fromValue * 3.5274
+        default:
+            return fromValue
+        }
+    }
+    
+    func convertStone(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "g":
+            return fromValue * 6350.29
+        case "kg":
+            return fromValue * 6.35029
+        default:
+            return fromValue
+        }
+    }
+
+    func convertPound(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "g":
+            return fromValue * 453.592
+        case "kg":
+            return fromValue * 0.453592
+        default:
+            return fromValue
+        }
+    }
+
+    func convertOunce(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "g":
+            return fromValue * 28.3495
+        case "kg":
+            return fromValue * 0.0283495
+        default:
+            return fromValue
+        }
+    }
+}
+
+struct Length: Measurement {
+    var header: String {
+        return "Length"
+    }
+    
+    var SIValues: [NSDictionary] {
+        return [["name": "Millimeter", "abbreviation": "mm"],
+                ["name": "Centimeter", "abbreviation": "cm"],
+                ["name": "Meter", "abbreviation": "m"],
+                ["name": "Kilometer", "abbreviation": "km"]]
+    }
+    
+    var imperialValues: [NSDictionary] {
+        return [["name": "Inch", "abbreviation": "\""],
+                ["name": "Foot", "abbreviation": "'"],
+                ["name": "Yard", "abbreviation": "yd"],
+                ["name": "Mile", "abbreviation": "mi"]]
+    }
+
+    internal func convert(fromUnit: String, toUnit: String, fromValue: Double) -> Double {
+        switch fromUnit {
+        case "mm":
+            return convertMillimeter(toUnit: toUnit, fromValue: fromValue)
+        case "cm":
+            return convertCentimeter(toUnit: toUnit, fromValue: fromValue)
+        case "m":
+            return convertMeter(toUnit: toUnit, fromValue: fromValue)
+        case "km":
+            return convertKilometer(toUnit: toUnit, fromValue: fromValue)
+        case "\"":
+            return convertInch(toUnit: toUnit, fromValue: fromValue)
+        case "'":
+            return convertFoot(toUnit: toUnit, fromValue: fromValue)
+        case "yd":
+            return convertYard(toUnit: toUnit, fromValue: fromValue)
+        case "mi":
+            return convertMile(toUnit: toUnit, fromValue: fromValue)
+        default:
+            print("NO CONVERSION FOR \(fromUnit). Plz implement!")
+            return fromValue
+        }
+    }
+
+    func convertMillimeter(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "\"":
+            return fromValue * 0.0393701
+        case "'":
+            return fromValue * 0.00328084
+        case "yd":
+            return fromValue * 0.00109361
+        case "mi":
+            return fromValue * 6.2137e-7
+        default:
+            return fromValue
+        }
+    }
+
+    func convertCentimeter(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "\"":
+            return fromValue * 0.393701
+        case "'":
+            return fromValue * 0.0328084
+        case "yd":
+            return fromValue * 0.0109361
+        case "mi":
+            return fromValue * 6.2137e-6
+        default:
+            return fromValue
+        }
+    }
+
+    func convertMeter(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "\"":
+            return fromValue * 3.93701
+        case "'":
+            return fromValue * 0.328084
+        case "yd":
+            return fromValue * 0.109361
+        case "mi":
+            return fromValue * 0.000062137
+        default:
+            return fromValue
+        }
+    }
+
+    func convertKilometer(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "\"":
+            return fromValue * 39370.1
+        case "'":
+            return fromValue * 3280.84
+        case "yd":
+            return fromValue * 1093.61
+        case "mi":
+            return fromValue * 0.62137
+        default:
+            return fromValue
+        }
+    }
+
+    func convertInch(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "mm":
+            return fromValue * 25.4
+        case "cm":
+            return fromValue * 2.54
+        case "m":
+            return fromValue * 0.0254
+        case "km":
+            return fromValue * 2.54e-5
+        default:
+            return fromValue
+        }
+    }
+
+    func convertFoot(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "mm":
+            return fromValue * 304.8
+        case "cm":
+            return fromValue * 30.48
+        case "m":
+            return fromValue * 0.3048
+        case "km":
+            return fromValue * 0.0003048
+        default:
+            return fromValue
+        }
+    }
+
+    func convertYard(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "mm":
+            return fromValue * 914.4
+        case "cm":
+            return fromValue * 91.44
+        case "m":
+            return fromValue * 0.9144
+        case "km":
+            return fromValue * 0.0009144
+        default:
+            return fromValue
+        }
+    }
+
+    func convertMile(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "mm":
+            return fromValue * 1.609e+6
+        case "cm":
+            return fromValue * 160934
+        case "m":
+            return fromValue * 1609.34
+        case "km":
+            return fromValue * 1.60934
+        default:
+            return fromValue
+        }
+    }
+
+}
+
+struct Volume: Measurement {
+    var header: String {
+        return "Volume"
+    }
+    
+    var SIValues: [NSDictionary] {
+        return [["name": "Millilitre", "abbreviation": "ml"],
+                ["name": "Litre", "abbreviation": "l"]]
+    }
+    
+    var imperialValues: [NSDictionary] {
+        return [["name": "Pint", "abbreviation": "pt"],
+                ["name": "Gallon", "abbreviation": "gal"]]
+    }
+
+    internal func convert(fromUnit: String, toUnit: String, fromValue: Double) -> Double {
+        switch fromUnit {
+        case "ml":
+            return convertMillilitre(toUnit: toUnit, fromValue: fromValue)
+        case "l":
+            return convertLitre(toUnit: toUnit, fromValue: fromValue)
+        case "pt":
+            return convertPint(toUnit: toUnit, fromValue: fromValue)
+        case "gal":
+            return convertGallon(toUnit: toUnit, fromValue: fromValue)
+        default:
+            print("NO CONVERSION FOR \(fromUnit). Plz implement!")
+            return fromValue
+        }
+    }
+
+    func convertMillilitre(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "pt":
+            return fromValue * 0.00211338
+        case "gal":
+            return fromValue * 0.000264172
+        default:
+            return fromValue
+        }
+    }
+
+    func convertLitre(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "pt":
+            return fromValue * 2.11338
+        case "gal":
+            return fromValue * 0.264172
+        default:
+            return fromValue
+        }
+    }
+
+    func convertPint(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "ml":
+            return fromValue * 473.176
+        case "l":
+            return fromValue * 0.473176
+        default:
+            return fromValue
+        }
+    }
+
+    func convertGallon(toUnit: String, fromValue: Double) -> Double {
+        switch toUnit {
+        case "ml":
+            return fromValue * 3785.41
+        case "l":
+            return fromValue * 3.78541
+        default:
+            return fromValue
+        }
+    }
+}
 
 class ModelController: NSObject, UIPageViewControllerDataSource {
 
-    var pageData: [String] = []
-
+    var pageData: [Measurement] = []
 
     override init() {
         super.init()
-        // Create the data model.
-        let dateFormatter = DateFormatter()
-        pageData = dateFormatter.monthSymbols
+        pageData = [Temperature(), Weight(), Length(), Volume()]
     }
 
     func viewControllerAtIndex(_ index: Int, storyboard: UIStoryboard) -> DataViewController? {
-        // Return the data view controller for the given index.
         if (self.pageData.count == 0) || (index >= self.pageData.count) {
             return nil
         }
 
-        // Create a new view controller and pass suitable data.
         let dataViewController = storyboard.instantiateViewController(withIdentifier: "DataViewController") as! DataViewController
         dataViewController.dataObject = self.pageData[index]
         return dataViewController
     }
 
     func indexOfViewController(_ viewController: DataViewController) -> Int {
-        // Return the index of the given data view controller.
-        // For simplicity, this implementation uses a static array of model objects and the view controller stores the model object; you can therefore use the model object to identify the index.
-        return pageData.index(of: viewController.dataObject) ?? NSNotFound
+        if let measurement = viewController.dataObject {
+            return pageData.index(where: { (m: Measurement) -> Bool in
+                m.header == measurement.header
+            })!
+        } else {
+            return NSNotFound
+        }
     }
 
     // MARK: - Page View Controller Data Source
